6107ad6c258655e82068d009f73baeadd9e112ee
Ubuntu <ubuntu@localhost.localdomain>
2026-02-22 22:21:39 +0800

    fix: 修正倒地旋转方向为顺时针 90 度

--- js/game/Renderer.js
+++ js/game/Renderer.js
@@ -139,7 +139,7 @@ class Renderer {
   // 应用动画偏移
   _applyAnimationOffset(ctx, player) {
     if (player.falling || player.fallenAngle > 0) {
-      ctx.rotate(Utils.degToRad(player.fallenAngle));
+      ctx.rotate(Utils.degToRad(-player.fallenAngle));
       return;
     }
     
